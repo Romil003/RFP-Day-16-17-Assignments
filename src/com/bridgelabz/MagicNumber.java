e2d7e6e62a0231fbb799ac3771454e26381ea79b
Romil Ramesh Ghadge <111551511+Romil003@users.noreply.github.com>
2022-11-02 21:38:51 +0530

    Added Magic Number Problem

--- src/com/bridgelabz/MagicNumber.java
+++ src/com/bridgelabz/MagicNumber.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class MagicNumber {
     public static void main(String[] args) {
         int result=0;
-        int smallestNumber = 0;
+        int smallestNumber = 0 ;
         int biggestNumber =100;
         Scanner scanner = new Scanner(System.in);
         do {
